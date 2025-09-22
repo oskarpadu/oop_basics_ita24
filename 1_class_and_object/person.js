2c0d0b53c91d4dafed806f53b2cef3f7ad7980e2
oskar <oskarpadu31@gmail.com>
2025-09-22 15:21:23 +0300

    add BMI calculation

--- 1_class_and_object/person.js
+++ 1_class_and_object/person.js
@@ -1,8 +1,54 @@
 class Person {
     name = ""
     age = 0
+    kaal = 0 
+    pikkus = 0
 
-    constructor(name) {
+    getPikkus() {
+        return this.pikkus
+    }
+
+    setPikkus(pikkus) {
+        if (pikkus < 0) {
+            console.log("Height cannot be negative.")
+            return
+        } else {        
+        this.pikkus = pikkus
+        }
+    }
+
+    getkaal() {
+        return this.kaal
+    }
+
+    setkaal(kaal) {
+        if (kaal < 0) {
+            console.log("Weight cannot be negative.")
+            return
+        } else {        
+        this.kaal = kaal
+        }
+    }
+
+    becomeHeavier() {
+        this.kaal++
+    }
+
+    isfat() {
+        if (this.kaal < 100) {
+            return false
+        }
+        return true
+    }
+
+    ispikk() {  
+        if (this.pikkus < 180) {
+            return false
+        }
+        return true
+    }
+
+    constructor(name, pikkus, kaal ) {
         this.name = name
     }
 
@@ -18,7 +64,12 @@ class Person {
     }
 
     printPerson() { 
-        console.log(`name: ${this.name}, age: ${this.age}`)
+        console.log(`name: ${this.name}, age: ${this.age} weight: ${this.kaal} height: ${this.pikkus} BMI: ${this.BMI().toFixed(2)}`)
+    }
+
+    BMI() {
+        const heightInMeters = this.pikkus / 100;
+        return this.kaal / (heightInMeters * heightInMeters);
     }
 
     getName() {
--- 1_class_and_object/persons.js
+++ 1_class_and_object/persons.js
@@ -5,26 +5,60 @@ const bella = new Person("Bella")
 
 const persons = [bob, brian, bella]
 
-bob.printPerson()
-brian.printPerson()
-bella.printPerson()
+while (bella.getPikkus() < 170) {
+    bella.setPikkus(bella.getPikkus() + 1)
+}
+
+for (let i = 0; i < 190; i++) {
+    brian.setPikkus(brian.getPikkus() + 1)
+}
+
+bob.setPikkus(160)
+
+while (bob.getkaal() < 100) {
+    bob.becomeHeavier()
+}
+
+for (let i = 0; i < 50; i++) {
+    brian.becomeHeavier()
+}
+
+bella.setkaal(45)
 
 while (bob.getAge() < 15) {
     bob.becomeOlder()
 }
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 6; i++) {
     brian.becomeOlder()
 }
+
+bella.setAge(18)
 
 bob.printPerson()
 brian.printPerson()
 bella.printPerson()
 
 persons.forEach(person => {
+    if (person.ispikk()) {
+    console.log(`${person.getName()} is tall.`)
+    } else {
+        console.log(`${person.getName()} is not tall.`)
+    }
+})
+
+persons.forEach(person => {
     if (person.isadult()) {
     console.log(`${person.getName()} is an adult.`)
     } else {
         console.log(`${person.getName()} is not an adult.`)
+    }
+})
+
+persons.forEach(person => {
+    if (person.isfat()) {
+    console.log(`${person.getName()} is fat.`)
+    } else {
+        console.log(`${person.getName()} is not fat.`)
     }
 })
