0d4c5b8cf405c3b9bdbc28ff6a69718e3b94b4c8
oskar <oskarpadu31@gmail.com>
2025-09-22 12:19:51 +0300

    create Person basic class and its objects

---
+++ 1_class_and_object/person.js
@@ -0,0 +1,22 @@
+export class Person {
+    name = ""
+    age = 0
+
+    setName(name) {
+        this.name = name
+    }
+
+    getName() {
+        return this.name
+    }
+
+    setAge(age)  {
+        this.age = age
+    }
+
+    getAge() {
+        return this.age
+    }
+} 
+
+module.exports = { Person }
---
+++ 1_class_and_object/persons.js
@@ -0,0 +1,5 @@
+const Person = require('./person.js').Person
+const bob = new Person()
+console.log(bob.age)
+console.log("name: " + bob.name)
+console.log(`age: ${bob.name}`)  
